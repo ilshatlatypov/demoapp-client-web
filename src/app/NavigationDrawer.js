92c9feefc6126fd1f3fc8f4859b1c2b77562d8be
Ilshat Latypov <latypov.ilshat@gmail.com>
2016-10-05 15:39:20 +0300

    make navigation menu selectable

--- src/app/NavigationDrawer.js
+++ src/app/NavigationDrawer.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, {Component, PropTypes} from 'react';
 import Drawer from 'material-ui/Drawer';
-import {List, ListItem} from 'material-ui/List';
+import {List, ListItem, MakeSelectable} from 'material-ui/List';
 import ContentPaste from 'material-ui/svg-icons/content/content-paste';
 import Divider from 'material-ui/Divider';
 
 import SocialPeople from 'material-ui/svg-icons/social/people';
 import ActionExitToApp from 'material-ui/svg-icons/action/exit-to-app';
+
+let SelectableList = MakeSelectable(List);
+
+function wrapState(ComposedComponent) {
+  return class SelectableList extends React.Component {
+    static propTypes = {
+      children: PropTypes.node.isRequired,
+      defaultValue: PropTypes.number.isRequired,
+    };
+
+    componentWillMount() {
+      this.setState({
+        selectedIndex: this.props.defaultValue,
+      });
+    }
+
+    handleRequestChange = (event, index) => {
+      this.setState({
+        selectedIndex: index,
+      });
+    };
+
+    render() {
+      return (
+        <ComposedComponent
+          value={this.state.selectedIndex}
+          onChange={this.handleRequestChange}
+        >
+          {this.props.children}
+        </ComposedComponent>
+      );
+    }
+  };
+}
+
+SelectableList = wrapState(SelectableList);
 
 export default class NavigationDrawer extends React.Component {
 
@@ -30,10 +66,10 @@ export default class NavigationDrawer extends React.Component {
         open={this.state.open} 
         onRequestChange={(open) => this.setState({open})}
       >
-        <List>
-          <ListItem primaryText="Задачи" leftIcon={<ContentPaste />} onTouchTap={() => this.itemSelected("tasks")} />
-          <ListItem primaryText="Сотрудники" leftIcon={<SocialPeople />} onTouchTap={() => this.itemSelected("users")} />
-        </List>
+        <SelectableList defaultValue={0}>
+          <ListItem value={0} primaryText="Задачи" leftIcon={<ContentPaste />} onTouchTap={() => this.itemSelected("tasks")} />
+          <ListItem value={1} primaryText="Сотрудники" leftIcon={<SocialPeople />} onTouchTap={() => this.itemSelected("users")} />
+        </SelectableList>
         <Divider />
         <List>
           <ListItem primaryText="Выйти" leftIcon={<ActionExitToApp />} onTouchTap={() => this.itemSelected("exit")} />

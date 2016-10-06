478eda7d8a572eaa4d338025e3ac92517708916c
Ilshat Latypov <latypov.ilshat@gmail.com>
2016-10-06 09:46:35 +0300

    Main refactoring

--- src/app/Main.js
+++ src/app/Main.js
@@ -1,85 +1,158 @@
-/**
- * In this file, we create a React component
- * which incorporates components providedby material-ui.
- */
-import React, {Component} from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
-import Dialog from 'material-ui/Dialog';
-import {deepOrange500} from 'material-ui/styles/colors';
-import {red700} from 'material-ui/styles/colors';
-import FlatButton from 'material-ui/FlatButton';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import React, {PropTypes} from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBar from 'material-ui/AppBar';
+import AppBarWithMenu from './AppBarWithMenu';
+import ToolbarExamplesSimple from './ToolbarExamplesSimple';
+import NavigationDrawer from './NavigationDrawer';
 
-const styles = {
-  container: {
-    textAlign: 'center',
-    paddingTop: 200,
-  },
-};
+import PaperWithList from './PaperWithList';
+import Paper from 'material-ui/Paper';
+import CircularProgress from 'material-ui/CircularProgress'
+import Divider from 'material-ui/Divider'
+import {List, ListItem} from 'material-ui/List';
+import client from './client'
+
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add'
+
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {
+  blueGrey500, blueGrey700,deepOrangeA200
+} from 'material-ui/styles/colors';
 
 const muiTheme = getMuiTheme({
   palette: {
-    accent1Color: red700,
-  },
+    primary1Color: blueGrey500,
+    primary2Color: blueGrey700,
+    accent1Color: deepOrangeA200,
+  }
 });
 
-class Main extends Component {
-  constructor(props, context) {
-    super(props, context);
+const fabStyle = {
+    margin: 0,
+    top: 'auto',
+    right: 20,
+    bottom: 20,
+    left: 'auto',
+    position: 'fixed',
+};
 
-    this.handleRequestClose = this.handleRequestClose.bind(this);
-    this.handleTouchTap = this.handleTouchTap.bind(this);
-
-    this.state = {
-      open: false,
-    };
+class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = { mainComponentName: "tasks" };
   }
 
-  handleRequestClose() {
-    this.setState({
-      open: false,
-    });
+  getChildContext() {
+    return {muiTheme: getMuiTheme(muiTheme)};
   }
 
-  handleTouchTap() {
-    this.setState({
-      open: true,
-    });
+  openDrawer = () => this.refs.drawer.handleToggle();
+
+  setMainComponent = (newMainComponentName) => {
+    this.setState({ mainComponentName: newMainComponentName});
   }
 
   render() {
-    const standardActions = (
-      <FlatButton
-        label="Ok"
-        primary={true}
-        onTouchTap={this.handleRequestClose}
-      />
-    );
+    var title;
+    var mainComponent;
+    if (this.state.mainComponentName === "tasks") {
+      mainComponent = <TaskList />;
+      title = "Задачи";
+    } else if (this.state.mainComponentName === "users") {
+      title = "Сотрудники";
+      mainComponent = <UserList />;
+    }
 
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
-        <div style={styles.container}>
-          <Dialog
-            open={this.state.open}
-            title="Super Secret Password"
-            actions={standardActions}
-            onRequestClose={this.handleRequestClose}
-          >
-            1-2-3-4-5
-          </Dialog>
-          <h1>Material-UI</h1>
-          <h2>example project</h2>
-          <RaisedButton
-            label="Super Secret Password"
-            secondary={true}
-            onTouchTap={this.handleTouchTap}
-          />
+        <div>
+          <ToolbarExamplesSimple title={title} onMenuIconButtonTouchTap={this.openDrawer} />
+          <NavigationDrawer ref="drawer" onItemSelected={this.setMainComponent} />
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12">
+                {mainComponent}
+              </div>
+            </div>
+          </div>
+          <FloatingActionButton secondary={true} style={fabStyle}>
+            <ContentAdd />
+          </FloatingActionButton>
         </div>
       </MuiThemeProvider>
-    );
+    )
   }
-}
+};
+
+Main.childContextTypes = {
+  muiTheme: PropTypes.object.isRequired,
+};
+
+class UserList extends React.Component {
+  constructor() {
+    super();
+    this.state = {users: [], showResults: false};
+  }
+
+  componentDidMount = () => 
+    client({method: 'GET', path: 'http://localhost:8080/users?sort=firstname&sort=lastname'}).then(response => {
+      this.setState({users: response.entity._embedded.users, showResults: true});
+    });
+
+  render() {
+      var users = this.state.users.map(user =>
+        <div>
+          <ListItem key={user._links.self.href} primaryText={user.firstname + " " + user.lastname} />
+          <Divider />
+        </div>
+      );
+      return (
+        <Paper>
+          { this.state.showResults ? <List>{users}</List> : <CircularProgress/> }
+        </Paper>
+      )
+  }
+};
+
+class TaskList extends React.Component {
+  constructor() {
+    super();
+    this.state = {tasks: [], contentMode: 0};
+  }
+
+  componentDidMount = () => 
+    client({method: 'GET', path: 'http://localhost:8080/tasks?projection=withUser&sort=date'}).then(response => {
+      this.setState({tasks: response.entity._embedded.tasks, contentMode: 1});
+    }, errorResponse => {
+      this.setState({tasks: [], contentMode: 2});
+    });
+
+  render() {
+    var tasks = this.state.tasks.map(task => {
+      var userFullname = (task.user != null ? task.user.firstname + " " + task.user.lastname : "не назначен");
+      return (<div key={task._links.self.href} >
+        <ListItem 
+          primaryText={task.title} 
+          secondaryText={"Исполнитель: " + userFullname}/>
+        <Divider />
+      </div>)
+    });
+    var component;
+    switch(this.state.contentMode) {
+      case 0:
+        component = <CircularProgress/>;
+        break;
+      case 1:
+        component = <List>{tasks}</List>;
+        break;
+      case 2:
+        component = <div>Ошибка</div>;
+        break;
+    }
+    return (
+      <Paper>{component}</Paper>
+    )
+  }
+};
 
 export default Main;
--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import Main from './Main'; // Our custom react component
-import NewMain from './NewMain';
+import Main from './Main';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
@@ -11,4 +10,4 @@ injectTapEventPlugin();
 // Render the main app react component into the app div.
 // For more details see: https://facebook.github.io/react/docs/top-level-api.html#react.render
 //render(<Main />, document.getElementById('app'));
-render(<NewMain />, document.getElementById('app'));
+render(<Main />, document.getElementById('app'));
